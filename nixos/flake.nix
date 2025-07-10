c114b3ce4015d257b1b109310057c377f890e339
Soorya Sanand <49944948+Sooryasanand@users.noreply.github.com>
2025-07-11 00:45:09 +1000

    Update flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -5,42 +5,18 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    polykey-cli.url = "github:MatrixAI/Polykey-CLI";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, home-manager, flake-utils, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        username = "sooryas";
-      in {
-        nixosConfigurations = {
-          macnix = nixpkgs.lib.nixosSystem {
-            inherit system;
-            modules = [
-              ./configuration.nix
-              home-manager.nixosModules.home-manager
-              {
-                home-manager.users.${username} = import ./home.nix;
-                users.users.${username}.isNormalUser = true;
-                users.users.${username}.extraGroups = [ "wheel" "networkmanager" ];
-                users.users.${username}.password = "password"; # CHANGE this!
-              }
-            ];
-            specialArgs = { inherit inputs system username; };
-          };
-        };
-
-        homeConfigurations = {
-          ${username} = home-manager.lib.homeManagerConfiguration {
-            pkgs = import nixpkgs {
-              inherit system;
-              config.allowUnfree = true;
-            };
-            extraSpecialArgs = { inherit inputs system username; };
-            modules = [ ./home.nix ];
-          };
-        };
-      }
-    ) // {};
+  outputs = { nixpkgs, home-manager, ... }: {
+    nixosConfigurations.macnix = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.users.sooryas = import ./home.nix;
+        }
+      ];
+    };
+  };
 }
